9c2d0bf4040accaeeccb93287ff4f709ddde2a41
Arkadiusz Zygan <a.zygan@gmail.com>
2020-02-17 22:37:16 +0100

    Add logout opt & hello

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/logged', (req, res) => {
-  res.render('logged');
+  res.render('logged', {username: req.user.displayName, image: req.user.photos});
+  
 });
+
 
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
