8dc52d90aaad4b949157744b435b8922484e3c6e
Arkadiusz Zygan <a.zygan@gmail.com>
2020-02-17 18:11:46 +0100

    Prepare to work

--- server.js
+++ server.js
@@ -2,8 +2,34 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
 
 const app = express();
+
+// configure passport provider options
+passport.use(new GoogleStrategy({
+  clientID: '717393282826-h4r66mn2cfmo4aamt0upccgm4onhenf7.apps.googleusercontent.com',
+  clientSecret: 'Na5kj96IOYbxnXEcya_CjDCl',
+  callbackURL: 'http://localhost:8000/auth/callback'
+}, (accessToken, refreshToken, profile, done) => {
+done(null, profile);
+}));
+
+// serialize user when saving to session
+passport.serializeUser((user, serialize) => {
+  serialize(null, user);
+});
+
+// deserialize user when reading from session
+passport.deserializeUser((obj, deserialize) => {
+  deserialize(null, obj);
+});
+
+app.use(session({ secret: 'gladiator' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
